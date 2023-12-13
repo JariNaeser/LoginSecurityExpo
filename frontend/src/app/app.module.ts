8631badc03406bb31667e57bae73ae7a0c9d6851
Jari Näser <jari.naeser@supsi.ch>
2023-12-13 14:57:56 +0100

    fix: home and login UI

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -5,16 +5,51 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
 import { LoginComponent } from './components/login/login.component';
+import { LogoutComponent } from './components/logout/logout.component';
+import { DetailsComponent } from './components/details/details.component';
+import { RouterModule, Routes } from '@angular/router';
+
+// Routes
+const appRoutes: Routes = [
+  {
+    path: '', 
+    component: HomeComponent 
+  },
+  { 
+    path: 'home', 
+    component: HomeComponent 
+  },
+  { 
+    path: 'login', 
+    component: LoginComponent 
+  },
+  { 
+    path: 'logout', 
+    component: LogoutComponent 
+  },
+  { 
+    path: 'details', 
+    component: DetailsComponent, 
+    //canActivate: [CanActivateAdminGuard] 
+  },
+  { 
+    path: '**', 
+    component: LoginComponent 
+  }
+];
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    LoginComponent
+    LoginComponent,
+    LogoutComponent,
+    DetailsComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
